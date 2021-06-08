2176c71b19c3f00eeff7cb9f4da81baca2214b96
abad89 <abadalamenti89@gmail.com>
2021-06-08 01:12:22 -0400

    set up controlled inputs for AddDayForm

--- src/components/AddDayPage.js
+++ src/components/AddDayPage.js
@@ -1,10 +1,12 @@
 import { NavLink } from "react-router-dom"
+import AddDayForm from "./AddDayForm"
 
 function AddDayPage() {
   return (
     <div>
       <h1>Add New Day</h1>
       <NavLink to="/"><button>Home</button></NavLink>
+      <AddDayForm />
     </div>
   );
 }
