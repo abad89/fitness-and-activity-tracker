e8e3d0708cab3bcb0899a552f05a7448bc1b8d55
abad89 <abadalamenti89@gmail.com>
2021-06-08 01:42:52 -0400

    add redirect to submit button

--- src/components/AddDayForm.js
+++ src/components/AddDayForm.js
@@ -1,5 +1,7 @@
 import { useState } from "react";
+import { useHistory } from "react-router-dom";
 function AddDayForm() {
+    let history = useHistory();
   const [formData, setFormData] = useState({
     month: "",
     date: "",
@@ -17,48 +19,83 @@ function AddDayForm() {
   }
 
   function handleSubmit(e) {
-      e.preventDefault()
-      const newDay = {
-          month: formData.month,
-          date: formData.date,
-          year: formData.year,
-          steps: formData.steps,
-          sleepHours: formData.sleepHours,
-          sleepMinutes: formData.sleepMinutes,
-      };
-      fetch("http://localhost:3001/days", {
-          method: "POST",
-          headers: {
-              "Content-Type": "application/JSON",
-          },
-          body: JSON.stringify(newDay)      
-      })
+    e.preventDefault();
+    const newDay = {
+      month: formData.month,
+      date: parseInt(formData.date),
+      year: parseInt(formData.year),
+      steps: parseInt(formData.steps),
+      sleepHours: parseInt(formData.sleepHours),
+      sleepMinutes: parseInt(formData.sleepMinutes),
+    };
+    fetch("http://localhost:3001/days", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/JSON",
+      },
+      body: JSON.stringify(newDay),
+    })
       .then((r) => r.json())
       .then((r) => console.log(r))
+      .then(history.push("/"));
   }
 
   return (
     <div>
       <form onSubmit={handleSubmit}>
         <label>
-          Month<input type="text" name="month" value={formData.month} onChange={handleChange}></input>
+          Month
+          <input
+            type="text"
+            name="month"
+            value={formData.month}
+            onChange={handleChange}
+          ></input>
         </label>
         <label>
-          Date<input type="text" name="date" value={formData.date} onChange={handleChange}></input>
+          Date
+          <input
+            type="text"
+            name="date"
+            value={formData.date}
+            onChange={handleChange}
+          ></input>
         </label>
         <label>
-          Year<input type="text" name="year" value={formData.year} onChange={handleChange}></input>
+          Year
+          <input
+            type="text"
+            name="year"
+            value={formData.year}
+            onChange={handleChange}
+          ></input>
         </label>
         <label>
-          Steps Taken<input type="text" name="steps" value={formData.steps} onChange={handleChange}></input>
+          Steps Taken
+          <input
+            type="text"
+            name="steps"
+            value={formData.steps}
+            onChange={handleChange}
+          ></input>
         </label>
         <label>
           Time Slept (Hours)
-          <input type="text" name="sleepHours" value={formData.sleepHours} onChange={handleChange}></input>
+          <input
+            type="text"
+            name="sleepHours"
+            value={formData.sleepHours}
+            onChange={handleChange}
+          ></input>
         </label>
         <label>
           Time Slept (Minutes)
-          <input type="text" name="sleepMinutes" value={formData.sleepMinutes} onChange={handleChange}></input>
+          <input
+            type="text"
+            name="sleepMinutes"
+            value={formData.sleepMinutes}
+            onChange={handleChange}
+          ></input>
           <input type="submit" value="Submit"></input>
         </label>
       </form>
