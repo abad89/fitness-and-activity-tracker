b166b4a2f766ecae2f08674a412e7368ae219311
abad89 <abadalamenti89@gmail.com>
2021-10-12 05:15:51 -0400

    fix graph sizing for moile view

--- src/components/SleepChart.js
+++ src/components/SleepChart.js
@@ -5,24 +5,29 @@ import {
   XAxis,
   YAxis,
   Tooltip,
+  ResponsiveContainer
 } from "recharts";
 
 function SleepChart({ daysList }) {
   return (
-    <div className="mx-1 col-xl-5 col-xs-12 border border-3 border-primary">
-      <h3>Sleep Amount by Date</h3>
-      <LineChart
-        width={400}
-        height={250}
-        data={daysList}
-        style={{ margin: "auto" }}
-      >
-        <Line type="monotone" dataKey="sleep" stroke="#f47c3c" />
-        <CartesianGrid stroke="black" />
-        <Tooltip />
-        <XAxis dataKey="iso" />
-        <YAxis />
-      </LineChart>
+    <div className="mx-1 col-xl-5 col-xs-12 border border-3 border-primary ">
+        <h3>Sleep Amount by Date</h3>
+      <div  style={{height: "200px"}}>
+        <ResponsiveContainer>
+          <LineChart
+            width={400}
+            height={250}
+            data={daysList}
+            style={{ margin: "auto" }}
+          >
+            <Line type="monotone" dataKey="sleep" stroke="#f47c3c" />
+            <CartesianGrid stroke="black" />
+            <Tooltip />
+            <XAxis dataKey="iso" />
+            <YAxis />
+          </LineChart>
+        </ResponsiveContainer>
+      </div>
     </div>
   );
 }
--- src/components/StepsChart.js
+++ src/components/StepsChart.js
@@ -5,25 +5,30 @@ import {
   XAxis,
   YAxis,
   Tooltip,
+  ResponsiveContainer
 } from "recharts";
 
 function StepsChart({ daysList }) {
   return (
     <div className="mx-1 col-xl-5 col-md-12 border border-3 border-primary">
       <h3>Steps by Date</h3>
-        <LineChart
-          width={400}
-          height={250}
-          data={daysList}
-          style={{ margin: "auto" }}
-        >
-          <Line type="monotone" dataKey="steps" stroke="#f47c3c" />
-          <CartesianGrid stroke="black" />
-          <Tooltip />
-          <XAxis dataKey="iso" />
-          <YAxis />
-        </LineChart>
+      <div style={{height: "200px"}}>
+        <ResponsiveContainer>
+          <LineChart
+            width={300}
+            height={250}
+            data={daysList}
+            style={{ margin: "auto" }}
+          >
+            <Line type="monotone" dataKey="steps" stroke="#f47c3c" />
+            <CartesianGrid stroke="black" />
+            <Tooltip />
+            <XAxis dataKey="iso" />
+            <YAxis />
+          </LineChart>
+        </ResponsiveContainer>
       </div>
+    </div>
   );
 }
 
